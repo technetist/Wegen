0f6021ecd11cb9490e7af28bffb0c32bc0892f9f
Adrien Maranville <adrien@Adriens-MacBook-Pro.local>
2017-06-26 18:01:29 -0600

    Created Log in system and segue

---
+++ Wegen/RiderViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RiderViewController.swift
+//  Wegen
+//
+//  Created by Adrien Maranville on 6/26/17.
+//  Copyright © 2017 Adrien Maranville. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class RiderViewController: UIViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "logoutSegue" {
+            PFUser.logOut()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
